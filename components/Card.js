640963d47b37f87e1ca4982a92216e55397b452c
Rise Erpelding <rise@heysparkbox.com>
2021-12-31 21:48:44 -0500

    feat: add styles

--- components/Card.js
+++ components/Card.js
@@ -11,17 +11,17 @@ export default function Card(props) {
 
   if (currentIndex < activeDeck.length) {
     return (
-      <section>
-        <p>Card {currentIndex + 1} of {activeDeck.length}</p>
-        <p>{activeDeck[currentIndex]}</p>
-        <button onClick={incrementIndex}>Next Card</button>
+      <section className="card">
+        <p className="card__number">Card {currentIndex + 1} of {activeDeck.length}</p>
+        <p className="card__content">{activeDeck[currentIndex]}</p>
+        <button className="card__button--cta" onClick={incrementIndex}>Next Card</button>
       </section>
     );
   } else {
     return (
-      <section>
+      <section className="card">
         <p>All done!</p>
-        <button onClick={shuffleDecks}>Play again</button>
+        <button className="card__button--cta" onClick={shuffleDecks}>Play again</button>
       </section>
     );
   }
