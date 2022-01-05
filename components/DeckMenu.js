2263c0251371ec4af174622fff8c41681332ce5b
Rise Erpelding <rise@heysparkbox.com>
2022-01-05 01:31:22 -0500

    feat: use list rather than select menu

--- components/DeckMenu.js
+++ components/DeckMenu.js
@@ -1,20 +1,22 @@
 export default function DeckMenu(props) {
   const { activeDeckName, decks, updateDeck } = props;
   const deckNames = Object.keys(decks);
-  const listedDecks = deckNames.map((deckName, index) => (
-    <option value={deckName} key={index}>{deckName}</option>
-  ));
 
-  const changeDeck = (event) => {
-    updateDeck(event.target.value);
+  const changeDeck = (deck) => {
+    updateDeck(deck);
   }
+
+  // activeDeckName will be used to make the active deck look different in the menu
+
+  const deckList = deckNames.map((deckName, index) => (
+    <li key={index} onClick={() => changeDeck(deckName)} className="nav__list-item">{deckName}</li>
+  ));
 
   return (
     <div className="nav__menu">
-      <label htmlFor="deck-select">Choose a deck:</label>
-      <select defaultValue={activeDeckName} name="decks" id="deck-select" onChange={changeDeck}>
-        {listedDecks}
-      </select>
+      <ul className="nav__list">
+        {deckList}
+      </ul>
     </div>
   )
 }
